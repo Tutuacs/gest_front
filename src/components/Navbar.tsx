0f388889c0d6a82e6198c5c0e62bc0c666cc198a
Tutuacs <arthursilva.rs1@gmail.com>
2025-05-19 10:04:20 -0300

    :sparkles: init

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+export default function Navbar() {
+    return (
+        <nav className="bg-gray-800 p-4">
+            <div className="justify-between flex w-full px-32">
+                <div className="text-white text-2xl">
+                    Home
+                </div>
+                <div className="text-white text-2xl">
+                    users
+                </div>
+            </div>
+        </nav>
+    )
+}
