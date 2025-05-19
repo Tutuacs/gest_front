5c3709c967be7e449886c1dfb0fda7445dbbfc9b
Tutuacs <arthursilva.rs1@gmail.com>
2025-05-19 10:42:22 -0300

    :boom: fix: navigation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,15 @@
+import Link from "next/link";
+
 export default function Navbar() {
     return (
         <nav className="bg-gray-800 p-4">
             <div className="justify-between flex w-full px-32">
-                <div className="text-white text-2xl">
+                <Link href="/" className="text-white text-2xl">
                     Home
-                </div>
-                <div className="text-white text-2xl">
+                </Link>
+                <Link href="/users" className="text-white text-2xl">
                     users
-                </div>
+                </Link>
             </div>
         </nav>
     )
